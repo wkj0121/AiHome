9ef28349ff01a0b5e10fb08829d9084d7b08b501
wkj <wkj_0121@126.com>
2018-01-27 23:14:08 +0800

    Ignore some files!

---
+++ AiHome/Comm/WebView/WebRouterConstant.h
@@ -0,0 +1,19 @@
+//
+//  WebRouterConstant.h
+//  AiHome
+//
+//  Created by wkj on 2018/1/27.
+//  Copyright © 2018年 华通晟云. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#pragma mark - web URL 路由表
+FOUNDATION_EXTERN NSString *const WEBMainURL;//主界面
+FOUNDATION_EXTERN NSString *const WEBAiHomeURL;//家庭设备界面
+FOUNDATION_EXTERN NSString *const WEBTimerSettingURL;//温控器设置界面
+FOUNDATION_EXTERN NSString *const WEBMessageURL;//提示信息界面
+FOUNDATION_EXTERN NSString *const WEBSettings_DeviceRegisterURL;//个人信息-设备登记
+FOUNDATION_EXTERN NSString *const WEBSettings_HouseRegisterURL;//个人信息-住宅登记
+FOUNDATION_EXTERN NSString *const WEBAddDeviceURL;//添加设备
+
